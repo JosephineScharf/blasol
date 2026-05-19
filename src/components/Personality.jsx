1ac1731545d527b10b731c74abe2a5221359332f
Katrina Bekere-Brivkalne <eaa25kbb@students.eaaa.dk>
2026-05-19 22:59:25 +0200

    Installed the tailwind, hopefully

---
+++ src/components/Personality.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
+export default function Personality() {
+    return (
+        <div>
+            <Header />
+            <h1>Personality</h1>
+            <Footer />
+        </div>
+    )
+}
+

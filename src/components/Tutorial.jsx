af0d114ea1e87a85137298ecc6e463933fefbe04
Julia <eaa25juma@students.eaaa.dk>
2026-05-20 11:21:43 +0200

    tutorial is working hallelujah

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -4,6 +4,7 @@ import banner1 from '../assets/tutorial/banner1.png'
 import banner2 from '../assets/tutorial/banner_2.png'
 import banner3 from '../assets/tutorial/banner3.png'
 import HomeImages from '../assets/tutorial/artists.png'
+import Modal from './Slides'
 
 
 const TARGET_DATE = new Date(2026, 5, 6, 0, 0, 0)
